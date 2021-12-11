d08ff66f5c9cd578a18a417b4cce0d7a743f3555
pr_pr <pr_pr@DESKTOP-HA4M2AI>
2021-12-11 07:59:02 +0530

    Add project files.

---
+++ Remove Patches V0.1/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Remove_Patches_V0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CheckBox = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.KB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.InstalledOn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.Color.Yellow;
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.Color.Yellow;
+            this.dateTimePicker1.Location = new System.Drawing.Point(282, 44);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.dateTimePicker1.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 32);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(282, 98);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show Patches";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Patch Installed Date";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CheckBox,
+            this.KB,
+            this.InstalledOn,
+            this.Description});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 170);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1023, 247);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // CheckBox
+            // 
+            this.CheckBox.HeaderText = "";
+            this.CheckBox.MinimumWidth = 8;
+            this.CheckBox.Name = "CheckBox";
+            this.CheckBox.Width = 150;
+            // 
+            // KB
+            // 
+            this.KB.HeaderText = "KB";
+            this.KB.MinimumWidth = 8;
+            this.KB.Name = "KB";
+            this.KB.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.KB.Width = 150;
+            // 
+            // InstalledOn
+            // 
+            this.InstalledOn.HeaderText = "InstalledOn";
+            this.InstalledOn.MinimumWidth = 8;
+            this.InstalledOn.Name = "InstalledOn";
+            this.InstalledOn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.InstalledOn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.InstalledOn.Width = 150;
+            // 
+            // Description
+            // 
+            this.Description.HeaderText = "Description";
+            this.Description.MinimumWidth = 8;
+            this.Description.Name = "Description";
+            this.Description.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Description.Width = 150;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(142, 424);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(628, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Uninstall Selected Patches";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 665);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1017, 27);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox1.Location = new System.Drawing.Point(11, 486);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.groupBox1.Size = new System.Drawing.Size(1017, 150);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Status";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(875, 131);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Select All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1037, 694);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Remove Patches v0.1 pr_prakas78@outlook.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn CheckBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KB;
+        private System.Windows.Forms.DataGridViewTextBoxColumn InstalledOn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Remove Patches V0.1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace Remove_Patches_V0._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Enabled = false;
+            button1.Enabled = false;
+            DateTime InstalledOn=DateTime.Parse(dateTimePicker1.Value.ToString());
+            String SelectedDate = $"{InstalledOn.Date.ToString("M/d/yyyy").Replace("-", "/")}";
+            // MessageBox.Show($"Patches Installed on {SelectedDate}");
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("\\\\.\\root\\cimv2", $"SELECT * FROM Win32_QuickFixEngineering"))
+            {
+                ManagementObjectCollection Patches = searcher.Get();
+                foreach (ManagementObject patch in Patches)
+                {
+
+                    //MessageBox.Show(patch["InstalledOn"].ToString());
+                    if (patch["InstalledOn"].ToString() == SelectedDate)
+                    {
+                        dataGridView1.Rows.Add(null, patch["HotFixID"], patch["InstalledOn"], patch["Description"]);
+                    }
+                }
+            }
+            if(dataGridView1.Rows.Count>0)
+            {
+                button2.Enabled = true;
+                button3.Enabled=true;
+            }
+            MessageBox.Show($"{dataGridView1.Rows.Count.ToString()} Patches for {SelectedDate}","",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button3.Enabled = false;
+            double counter = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Show();
+            int totalrows = dataGridView1.RowCount;
+            string message = string.Empty;
+            System.Diagnostics.Stopwatch st = new System.Diagnostics.Stopwatch();
+           
+            double totalCheckedBox = 0;
+            foreach (DataGridViewRow it in dataGridView1.Rows)
+            {
+
+                bool isSelected = Convert.ToBoolean(it.Cells["CheckBox"].Value);
+                if(isSelected)
+                {
+                    totalCheckedBox++;
+                }
+            }
+            st.Start();
+                foreach (DataGridViewRow item in dataGridView1.Rows)
+            {
+
+                bool isSelected = Convert.ToBoolean(item.Cells["CheckBox"].Value);
+                String cmd;
+                if(Environment.Is64BitOperatingSystem)
+                {
+                    cmd = "c:\\windows\\sysnative\\wusa.exe";
+                }
+                else
+                {
+                    cmd = "c:\\windows\\system32\\wusa.exe";
+                }
+                if (isSelected && totalCheckedBox>0)
+                {
+                    String PatchToInstall = item.Cells["KB"].Value.ToString().Replace("KB", "");
+                    //MessageBox.Show(PatchToInstall);
+                    using (System.Diagnostics.Process pr = new System.Diagnostics.Process())
+                    {
+                        System.Diagnostics.ProcessStartInfo prinfo = new System.Diagnostics.ProcessStartInfo(cmd, $"/uninstall /quiet /norestart /kb:{PatchToInstall}");
+                        pr.StartInfo = prinfo;
+                        pr.StartInfo.UseShellExecute = true;
+                        pr.StartInfo.CreateNoWindow = true;
+
+
+                        await Task.Run(() => {
+
+                            BeginInvoke(new Action(() =>
+                            {
+                                counter++;
+                              
+                                progressBar1.Value = (int)(counter /totalCheckedBox * 100);
+                                label2.Text = $"Status: Uninstalling {PatchToInstall} using wusa.exe\n{counter}\\{totalCheckedBox} done\nProgress: {progressBar1.Value} %\nETA:{TimeSpan.FromSeconds((st.Elapsed.TotalSeconds/counter)*(totalCheckedBox-counter)).ToString(@"hh\:mm\:ss")} ";
+                            }));
+                            pr.Start();
+                            pr.WaitForExit();
+
+                        });
+                        
+
+                      
+                       
+                            
+                        
+                       
+
+
+                    }
+
+                    //message += Environment.NewLine;
+                    //message += item.Cells["KB"].Value;
+                }
+
+
+            }
+                
+            label2.Text = $"Status:Completed\nTotal Time: {TimeSpan.FromSeconds( st.Elapsed.TotalSeconds).ToString(@"hh\:mm\:ss")} ";
+            st.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            foreach (DataGridViewRow itm in dataGridView1.Rows)
+            {
+                
+                itm.Cells["CheckBox"].Value = true;
+            }
+        }
+
+      
+    }
+}
